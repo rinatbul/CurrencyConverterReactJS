1311f41bce0508eac95cf33b478a1075680ae835
rinatbul <rinatbuldybayev@gmail.com>
2023-03-12 15:41:45 +0600

    currency-converter on react using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import {CurrencyRow} from "./CurrencyRow";
+import React, {useEffect, useState} from "react";
+
+let myHeaders = new Headers();
+myHeaders.append("apikey", "6eapaZ9jEYcFPd29RBLq3UJXM1bT6cI0");
+
+let requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+};
+const URL = 'https://api.apilayer.com/fixer/latest'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currencyOptions, setCurrencyOptions] = useState([])
+    const [fromCurrency, setFromCurrency] = useState()
+    const [toCurrency, setToCurrency] = useState()
+    const [exchangeRate, setExchangeRate] = useState()
+    const [amount, setAmount] = useState(1)
+    const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+    let toAmount, fromAmount
+    if (amountInFromCurrency) {
+        fromAmount = amount
+        toAmount = amount * exchangeRate
+    } else {
+        toAmount = amount
+        fromAmount = amount / exchangeRate
+    }
+
+    useEffect(() => {
+        fetch(URL, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                const firstCurrency = Object.keys(data.rates)[0]
+                setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+                setFromCurrency(data.base)
+                setToCurrency(firstCurrency)
+                setExchangeRate(data.rates[firstCurrency])
+            })
+            .catch(error => console.log('error', error));
+    }, [])
+
+    useEffect(() => {
+        if (fromCurrency !== null && toCurrency !== null) {
+            fetch(`${URL}?base=${fromCurrency}&symbols=${toCurrency}`, requestOptions)
+                .then(response => response.json())
+                .then(data => setExchangeRate(data.rates[toCurrency]))
+        }
+
+    }, [fromCurrency, toCurrency])
+
+    const handleFromAmountChange = (e) => {
+        setAmount(e.target.value)
+        setAmountInFromCurrency(true)
+    }
+    const handleToAmountChange = (e) => {
+        setAmount(e.target.value)
+        setAmountInFromCurrency(false)
+    }
+
+    return (
+        <div className="App">
+            <h1>Convert</h1>
+            <CurrencyRow currencyOptions={currencyOptions}
+                         selectCurrency={fromCurrency}
+                         onChangeCurrency={e => setFromCurrency(e.target.value)}
+                         onChangeAmount={handleFromAmountChange}
+                         amount={fromAmount}/>
+            <div className='equals'>=</div>
+            <CurrencyRow currencyOptions={currencyOptions}
+                         selectCurrency={toCurrency}
+                         onChangeCurrency={e => setToCurrency(e.target.value)}
+                         onChangeAmount={handleToAmountChange}
+                         amount={toAmount}/>
+        </div>
+    );
 }
 
 export default App;
